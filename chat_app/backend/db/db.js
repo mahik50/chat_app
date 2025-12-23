6521aecaac32912358189aa3c11884a3f886df0f
mahik50 <mahikesiyal15@gmail.com>
2025-12-23 13:50:30 +0530

    changed

--- chat_app/backend/db/db.js
+++ chat_app/backend/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://mahikesiyal:mahi1234@cluster0.uftuxtw.mongodb.net/?appName=Cluster0")
+    await mongoose.connect("mongodb+srv://mahikesiyal:mahi1234@cluster0.uftuxtw.mongodb.net/chat_app")
     .then(() => {
         console.log("Connected to DB");
     })
